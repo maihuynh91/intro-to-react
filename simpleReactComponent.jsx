7985a8f2a1b8301ec440f49956e40d576d3b5303
Mai Huynh <nhumaihuynh91@gmail.com>
2018-09-03 15:39:59 -0500

    Props and State Checkpoint Assignment

--- simpleReactComponent.jsx
+++ simpleReactComponent.jsx
@@ -1,9 +1,21 @@
 class FirstReact extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            users: [
+            {name: "Mai Huynh", age: 24}
+            ],
+            title: "Mai's page"
+        }
+    }
+
     render(){
         return(
             <div>
                 <p>Simple React Component.</p>
+                <p>{this.state.title}</p>
                 <ul>
+                    <li>Curent user: {this.state.users[0].name} and her age is {this.state.users[0].age} </li>
                     <li>Hello {this.props.name}</li>
                     <li>This is my {this.props.numb} React Page</li>  
                 </ul>
@@ -12,6 +24,6 @@ class FirstReact extends React.Component{
     }
 }
 ReactDOM.render(
-    <FirstReact name="Mai Huynh" numb ="first"/>, 
+    <FirstReact name="Mai Huynh" numb="first"/>, 
     document.getElementById("app")
 );
